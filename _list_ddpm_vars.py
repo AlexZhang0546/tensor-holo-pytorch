cb53ef8813b072972b5c090f398dc583b6846970
AlexZhang0546 <zrx070628@163.com>
2026-08-19 18:57:02 +0800

    fix: update variable handling in _list_ddpm_vars.py

--- _list_ddpm_vars.py
+++ _list_ddpm_vars.py
@@ -2,5 +2,5 @@ import tensorflow as tf
 r = tf.train.NewCheckpointReader("model/ckpt_full_loss_pitch_8_layers_30_filters_24_ldi_0_ddpm_12/ckpt-7695000")
 names = r.get_variable_to_shape_map()
 for k in sorted(names):
-    if "batch_normalization" in k.lower() or "Variable" in k:
+    if k.startswith("ddpm/"):
         print(k, names[k])
