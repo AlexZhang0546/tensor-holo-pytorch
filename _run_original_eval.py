b8d18959eb69aef128878b03a2cf6d8988371f0f
AlexZhang0546 <zrx070628@163.com>
2026-08-22 20:36:58 +0800

    feat: add two new lines to _run_original_eval.py

--- _run_original_eval.py
+++ _run_original_eval.py
@@ -4,6 +4,8 @@ import runpy
 
 import tensorflow as tf
 tf.compat.v1.disable_eager_execution()
+tf.layers = tf.compat.v1.layers
+tf.losses = tf.compat.v1.losses
 
 if __name__ == "__main__":
     # argparse in main_v2.py sees sys.argv[1:]; keep script name as argv[0].
