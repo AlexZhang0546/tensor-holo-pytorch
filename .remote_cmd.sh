238fc1e7b9932117762cb8a61cc0adc097730d3e
AlexZhang0546 <zrx070628@163.com>
2026-08-24 15:01:33 +0800

    fix: update remote command script with minor adjustments

--- .remote_cmd.sh
+++ .remote_cmd.sh
@@ -1,2 +1,2 @@
-cd /root/autodl-tmp/ZhangRuixuan/tensor-holo
-grep -n "def _build_graph\|data_format\|tf.nn.conv2d\|tf.compat.v1.layers.conv2d\|depth_to_space\|transpose" -A 70 main_v2.py | head -n 220
+cd /root/autodl-tmp/ZhangRuixuan/tensor-holo-clean
+/root/autodl-tmp/miniconda3/envs/tensorholo38/bin/python _eval_orig_clean.py
